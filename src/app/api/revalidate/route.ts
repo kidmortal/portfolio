0765e46e196bdd80fef25ea9223d000fb0c8a254
kidmortal <kidmortal@gmail.com>
2024-02-26 14:15:20 -0300

    revalidate webhook fix

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,8 +1,25 @@
 import { NextResponse } from "next/server";
-import { revalidateTag } from "next/cache";
+import { revalidatePath } from "next/cache";
+import { createClient } from "@/prismicio";
 
-export async function POST() {
-  revalidateTag("prismic");
+export async function POST(req: Request) {
+  const client = createClient();
+  const data = await req.json();
+  if (data.documents) {
+    const revalidatedPaths: string[] = [];
 
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+    for await (const doc of data.documents) {
+      const page = await client.getByID(doc);
+      const slug = page.type;
+      revalidatePath(slug);
+      revalidatedPaths.push(slug);
+    }
+    return NextResponse.json({
+      revalidated: true,
+      now: Date.now(),
+      paths: revalidatedPaths,
+    });
+  }
+
+  return NextResponse.json({ revalidated: false, message: "no slug provided" });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,11 @@ export default async function Page() {
   const page = await client.getSingle("home");
   return (
     <main>
+      <script
+        async
+        defer
+        src="https://static.cdn.prismic.io/prismic.js?new=true&repo=kidmortal"
+      ></script>
       <SliceZone slices={page.data.slices} components={components} />
     </main>
   );
