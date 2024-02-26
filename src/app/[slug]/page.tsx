4a316ba1a84a0d889a5d2a836a61c4f2ce3fe526
kidmortal <kidmortal@gmail.com>
2024-02-26 14:56:11 -0300

    change how to get param and fallback

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,7 +4,17 @@ import { components } from "@/slices";
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const client = createClient();
-  const page = await client.getByUID("page", params.slug);
-  console.log(`fetching ${params.slug}`);
-  return <SliceZone slices={page.data.slices} components={components} />;
+  try {
+    const page = await client.getByUID("page", params.slug);
+    console.log(`fetching ${params.slug}`);
+    return <SliceZone slices={page.data.slices} components={components} />;
+  } catch (error) {
+    return (
+      <div>
+        <h3 style={{ color: "white" }}>
+          Deu ruim, error {JSON.stringify(error)}
+        </h3>
+      </div>
+    );
+  }
 }
